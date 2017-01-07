5345babc6d65ddf49e87c0987d7468d15a61e4b6
Ansou1 <simondaguenet81@gmail.com>
2017-01-07 16:17:43 +0100

    first step

--- Tutoriel/Javascript/script.js
+++ Tutoriel/Javascript/script.js
@@ -1,7 +1,43 @@
 window.onload = function()
 {
-	var canvas = document.createElement('canvas');
-	canvas.width = 900;
-	canvas.height = 600;
-	canvas.style.border = "1px solid";
+	var canvasWidth = 900;
+	var canvasHeight = 600;
+	var blockSize = 30;
+	var ctx;
+	var delay = 1000;
+	var xCoord = 0;
+	var yCoord = 0;
+
+	init();
+
+	function init()
+	{
+		var canvas = document.createElement('canvas');
+		canvas.width = canvasWidth;
+		canvas.height = canvasHeight;
+		canvas.style.border = "1px solid";
+		document.body.appendChild(canvas);
+		ctx = canvas.getContext('2d');
+		refreshCanvas();
+	}
+
+	function refreshCanvas()
+	{
+		xCoord += 2;
+		yCoord += 2;
+		ctx.fillStyle = "#ff0000";
+		ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+		ctx.fillRect(xCoord, yCoord, 100, 50);
+		setTimeout(refreshCanvas, delay);
+	}
+
+
+	function Snake(body)
+	{
+		this.body = body;
+		this.draw = function()
+		{
+			
+		}
+	}
 }
