cece1dd38c320c5942063671653221f20dc305b3
Ansou1 <simondaguenet81@gmail.com>
2017-01-08 19:05:39 +0100

    En cours

--- Tutoriel/JQuerry/script.js
+++ Tutoriel/JQuerry/script.js
@@ -1,7 +1,27 @@
 $(function(){
 
-	$("#start").click(function() {
-		$("p").not(".special").css("border-color", "green");
+	var $mainMenuItems = $("#main-menu ul").children("li"),
+		totalMainMenuItems = $mainMenuItems.length,
+		openedIndex = -1,
+		init = function () {
 
-	})
+			$mainMenuItems.children(".images").click(function(){
+				var newIndex = $(this).parent().index();
+				if (openedIndex === newIndex) 
+				{
+					animateItem($item, true, 250);
+				}
+				$item = $mainMenuItems.eq(newIndex);
+				animateItem($item, true, 250);
+				openedIndex = newIndex;
+			});
+		},
+		animateItem = function($item, toOpen, speed){
+				var $colorImage = $item.find(".color"),
+				itemParam = toOpen ? {width:"420px"} : {width:"140px"},
+				colorImageParam = toOpen ? {left:"0px"} : {left:"140px"};
+				$colorImage.animate(colorImageParam, speed);
+				$item.animate(itemParam, speed);
+		};
+		init();
 });
