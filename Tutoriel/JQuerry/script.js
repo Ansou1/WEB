64a15773b1a6efaf3f819496297976537fb01464
Ansou1 <simondaguenet81@gmail.com>
2017-01-09 14:43:04 +0100

    Fin Jquerry / début bootstrap

--- Tutoriel/JQuerry/script.js
+++ Tutoriel/JQuerry/script.js
@@ -2,26 +2,65 @@ $(function(){
 
 	var $mainMenuItems = $("#main-menu ul").children("li"),
 		totalMainMenuItems = $mainMenuItems.length,
-		openedIndex = -1,
+		openedIndex = 2,
 		init = function () {
 
+			bindEvents();
+
+			if (validIndex(openedIndex))
+			{
+				animateItem($mainMenuItems.eq(openedIndex), true, 250);
+			}
+		},
+
+		bindEvents = function() {
 			$mainMenuItems.children(".images").click(function(){
 				var newIndex = $(this).parent().index();
-				if (openedIndex === newIndex) 
-				{
-					animateItem($item, true, 250);
-				}
-				$item = $mainMenuItems.eq(newIndex);
-				animateItem($item, true, 250);
-				openedIndex = newIndex;
+				checkAndAnimateItem(newIndex);
 			});
+
+			$(".button").hover(
+			function(){
+				$(this).addClass("hovered");
+			},
+			function(){
+				$(this).removeClass("hovered");
+			}
+			);
+
+			$(".button").click(function(){
+				var newIndex = $(this).index();
+				checkAndAnimateItem(newIndex);
+			});
+
+		}
+
+		validIndex = function(indexToCheck){
+			return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
 		},
+
 		animateItem = function($item, toOpen, speed){
 				var $colorImage = $item.find(".color"),
 				itemParam = toOpen ? {width:"420px"} : {width:"140px"},
 				colorImageParam = toOpen ? {left:"0px"} : {left:"140px"};
 				$colorImage.animate(colorImageParam, speed);
 				$item.animate(itemParam, speed);
+		},
+		checkAndAnimateItem = function(indexToCheckAnimate){
+				if (openedIndex === indexToCheckAnimate) 
+				{
+					animateItem($mainMenuItems.eq(indexToCheckAnimate), false, 250);
+					openedIndex = -1;
+				}
+				else
+				{
+					if (validIndex(indexToCheckAnimate))
+					{
+						animateItem($mainMenuItems.eq(openedIndex), false, 250);
+						openedIndex = indexToCheckAnimate;
+						animateItem($mainMenuItems.eq(openedIndex), true, 250);				
+					}
+				}
 		};
 		init();
 });
