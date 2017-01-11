cd08f8826d7693c423e398f1a0875603edd531dd
Ansou1 <simondaguenet81@gmail.com>
2017-01-11 23:37:35 +0100

    Final tutorial

--- Tutoriel/Bootstrap/JS/script.js
+++ Tutoriel/Bootstrap/JS/script.js
@@ -1,3 +1,8 @@
 $(function (){
-	$('[data-toggle="tooltip]').tooltip();
+	$(".navbar a, footer a").on("click", function(event){
+		event.preventDefault();
+		var hash = this.hash;
+
+		$('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+	});
 })
